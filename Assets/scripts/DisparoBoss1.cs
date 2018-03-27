215c12e232588bacbe7594ea3037b8a37b6929c8
Dankgard <dangar18@ucm.es>
2018-03-27 18:08:39 +0200

    Empezando nivel 1_3, primer boss

---
+++ Assets/scripts/DisparoBoss1.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisparoBoss1 : MonoBehaviour {
+
+    public bool playerInRange = false;
+    public GameObject misilePrefab;
+    public GameObject missileSpawner;
+    GameObject player;
+    public Transform enemyTrans;
+
+    public MovimientoBoss1 mov;
+
+    public float shootingRate;
+
+    // Use this for initialization
+    void Start () {
+        player = GameObject.FindWithTag("Player");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (playerInRange)
+        {
+
+        }
+
+    }
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            playerInRange = true;
+        }
+    }
+    public void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            playerInRange = false;
+        }
+    }
+
+    void Shoot()
+    {
+        Instantiate(misilePrefab, missileSpawner.transform.position, misilePrefab.transform.rotation);
+    }
+}
---
+++ Assets/scripts/MovimientoBoss1.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovimientoBoss1 : MonoBehaviour
+{
+
+    public GameObject startPoint;
+    public GameObject endPoint;
+
+    public bool colisionPlayer = false;
+
+    public float enemySpeed;
+    public float jumpForce;
+    bool isGoingRight;
+
+    public DisparoBoss1 rango;
+
+    SpriteRenderer sprite;
+    Rigidbody2D rb;
+
+    public float jumpProb = 0.2f;
+    float jumpNumber;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        sprite = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        jumpNumber = Random.Range(0, 1);
+
+        
+        if (rango.playerInRange)
+        {
+            if (!isGoingRight)
+            { 
+                if(jumpNumber < jumpProb)
+                    rb.AddForce(new Vector2 (-jumpForce, jumpForce));
+            }
+            else
+            {
+
+                if (jumpNumber < jumpProb)
+                    rb.AddForce(new Vector2 (jumpForce, jumpForce));
+            }
+        }
+
+
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            colisionPlayer = true;
+        }
+
+        if(collision.tag == "DirCollision")
+        {
+            if(!isGoingRight)
+            {
+                isGoingRight = true;
+                sprite.flipX = true;
+                rb.velocity = new Vector2(enemySpeed, 0);
+            }
+            else
+            {
+                isGoingRight = false;
+                sprite.flipX = false;
+                rb.velocity = new Vector2(-enemySpeed, 0);
+            }
+        }
+    }
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            colisionPlayer = false;
+        }
+    }
+}
