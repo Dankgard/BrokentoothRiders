b80d7e87052fd73e55382d41dbc0eea1df311a9c
GonzaPM7 <gonzap03@ucm.es>
2018-04-01 14:41:12 +0200

    Inicios del mapa Road to Skull Town y mejoras del comportamiento de la moto

--- Assets/scripts/MovimientoMotoProta.cs
+++ Assets/scripts/MovimientoMotoProta.cs
@@ -10,46 +10,57 @@ public class MovimientoMotoProta : MonoBehaviour {
     Rigidbody2D rb;
     public int NSaltos = 1;
     bool aumentar = true;
+    bool veloc = true;
+    
 	void Start () {
-        rb = GetComponent<Rigidbody2D>();
+        rb = GetComponent<Rigidbody2D>();        
 	}
-	
-	// Update is called once per frame
-	void Update () {
-        if (rb.velocity.y == 0)        
-            NSaltos = 1;
 
-        if (rb.velocity.x == 0)    
-            Destroy(gameObject);        
+    void FixedUpdate()
+    {               
 
-
-        if (Input.GetButtonDown("Jump"))
-        {
-            if (NSaltos > 0)
-            {
-                Vector2 s = new Vector2(rb.velocity.x, fuerza);
-                rb.velocity = s;
-                NSaltos -= 1;
-            }
-        }
-        else
-        {
-            Vector2 v = new Vector2(speed, rb.velocity.y);
-            rb.velocity = v;
-        }
         if (aumentar)
         {
             aumentar = false;
-            Invoke("AumentarVelocidad", tiempoaumneto);            
+            Invoke("AumentarVelocidad", tiempoaumneto);
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+        if (rb.velocity.y == 0)
+            NSaltos = 1;
+
+        if (Input.GetButtonDown("Jump") && veloc)
+        {
+            if (NSaltos > 0)
+            {                
+                Vector2 s = new Vector2(rb.velocity.x, fuerza);
+                rb.velocity = s;
+                NSaltos -= 1;                
+            }
+        }
+        else if (veloc)
+        {            
+            Vector2 v = new Vector2(speed, rb.velocity.y);
+            rb.velocity = v;            
+        }
+
+        if (rb.velocity.x == 0)
+        {
+            veloc = false;
+            Destroy(gameObject);          
+
         }
         
-        
-	}
+    }
     
     void AumentarVelocidad()
     {
         speed = speed + 1;
         aumentar = true;
     }
+    
 
 }
