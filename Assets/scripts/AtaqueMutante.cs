acf5de03fcabb1391d7430202fe6e3fa2bc368e6
Dankgard <dangar18@ucm.es>
2018-04-04 10:43:14 +0200

    Empezando con el mutante normal

---
+++ Assets/scripts/AtaqueMutante.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AtaqueMutante : MonoBehaviour
+{
+
+    public bool playerInRange = false;
+    public MovimientoMutante mov;
+    GameObject player;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        player = GameObject.FindWithTag("Player");
+    }
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            playerInRange = true;
+        }
+    }
+    public void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            playerInRange = false;
+        }
+    }
+
+}
---
+++ Assets/scripts/MovimientoMutante.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovimientoMutante : MonoBehaviour
+{
+
+    public GameObject startPoint;
+    public GameObject endPoint;
+
+    public bool colisionPlayer = false;
+
+    public float enemySpeed;
+    float speed;
+
+    bool isGoingRight;
+
+    public AtaqueMutante rango;
+
+    GameObject player;
+
+    void Awake()
+    {
+        rango = GetComponentInChildren<AtaqueMutante>();
+        speed = enemySpeed;
+    }
+
+    void Start()
+    {
+        player = GameObject.FindWithTag("Player");
+
+        if (!isGoingRight)
+        {
+            transform.position = startPoint.transform.position;
+        }
+        else
+        {
+            transform.position = endPoint.transform.position;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (rango.playerInRange == false)
+        {
+            if (!isGoingRight)
+            {
+
+                transform.localScale = new Vector3(1, 1, 1);
+
+
+                transform.position = Vector3.MoveTowards(transform.position, endPoint.transform.position, speed * Time.deltaTime);
+                if (transform.position == endPoint.transform.position)
+                {
+                    isGoingRight = true;
+                    transform.localScale = new Vector3(-1, 1, 1);
+                }
+
+            }
+            if (isGoingRight)
+            {
+                transform.localScale = new Vector3(-1, 1, 1);
+
+                transform.position = Vector3.MoveTowards(transform.position, startPoint.transform.position, speed * Time.deltaTime);
+                if (transform.position == startPoint.transform.position)
+                {
+                    isGoingRight = false;
+                    transform.localScale = new Vector3(1, 1, 1);
+
+                }
+
+            }
+        }
+        else
+        {
+            speed += 5;
+            if (player.transform.position.x <= transform.position.x)
+            {
+                transform.position = Vector3.MoveTowards(transform.position, endPoint.transform.position, speed * Time.deltaTime);
+            }
+            else if (player.transform.position.x >= transform.position.x)
+            {
+                transform.position = Vector3.MoveTowards(transform.position, startPoint.transform.position, speed * Time.deltaTime);
+            }
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            colisionPlayer = true;
+        }
+    }
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            colisionPlayer = false;
+        }
+    }
+}
+
