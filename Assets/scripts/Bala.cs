5c411e01b4905bff0a26424765512104d594d878
Dankgard <dangar18@ucm.es>
2018-04-08 16:05:09 +0200

    Terminado comportamiento boss 2

--- Assets/scripts/Bala.cs
+++ Assets/scripts/Bala.cs
@@ -8,18 +8,14 @@ public class Bala : MonoBehaviour {
 
     void OnTriggerEnter2D(Collider2D collider)
     {
-        if (collider.gameObject.tag == "Suelo")
+        if (collider.gameObject.tag == "Suelo" || collider.gameObject.tag == "EnemyBullet" || collider.gameObject.tag == "Proyectil")
             Destroy(gameObject);
-        else if (collider.gameObject.tag == "Enemigo")
+        else if (collider.gameObject.tag == "Enemigo" || collider.gameObject.tag == "Boss")
         {
             VidaEnemigo enemigo = collider.gameObject.GetComponent<VidaEnemigo>();
             enemigo.TakeDamage(bulletDamage);
             Destroy(gameObject);
         }
-        else if (collider.gameObject.tag == "EnemyBullet")
-            Destroy(gameObject);
-        else if(collider.gameObject.tag == "Proyectil")
-            Destroy(gameObject);
         else if (collider.gameObject.tag == "Caja")
         {
             Caja caja = collider.GetComponent<Caja>();
