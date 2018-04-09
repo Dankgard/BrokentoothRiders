bf40ac368ee65c99e210ed9a9317cb376c60ee92
GonzaPM7 <gonzap03@ucm.es>
2018-04-09 18:52:18 +0200

    Añadido script del comportamiento de los edificios

---
+++ Assets/scripts/ComportamientoEdificios.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ComportamientoEdificios : MonoBehaviour {
+    public GameObject endPoint;    
+    public float caida;
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+            transform.position = Vector3.MoveTowards(transform.position, new Vector2(transform.position.x, endPoint.transform.position.y), caida * Time.deltaTime);
+        
+       
+    }
+}
