42d957434c6a6de55278782935994652b6e7274c
Dankgard <dangar18@ucm.es>
2018-04-19 17:32:34 +0200

    Hecho movimiento del boss 5 en gran medida

--- Assets/scripts/DrakeMovimiento.cs
+++ Assets/scripts/DrakeMovimiento.cs
@@ -4,30 +4,44 @@ using UnityEngine;
 
 public class DrakeMovimiento : MonoBehaviour {
 
-    public int time;
-
     GameObject player;
 
-    public float throwTime;
+    public float teleportTime;
     float throwCount;
 
-	void Start () {
+    public GameObject teleporter;
+    public float teleportDestroyTime;
+
+    void Start () {
         player = GameObject.FindWithTag("Player");
 	}
 	
 	void Update () {
         throwCount += Time.deltaTime;
-        if (throwCount >= throwTime)
+        if (throwCount >= teleportTime)
         {
-            if (player.transform.position.x <= transform.position.x)
-            {
-
-            }
-            else if (player.transform.position.x >= transform.position.x)
-            {
-            }
+            Teleport();
             throwCount = 0;
         }
-		
-	}
+
+        if (player.transform.position.x <= transform.position.x)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+
+        }
+        else if (player.transform.position.x >= transform.position.x)
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+    }
+
+    void Teleport()
+    {
+        GameObject tp = Instantiate(teleporter, transform.position - new Vector3(0,0.5f,0), Quaternion.identity);
+        Destroy(tp, teleportDestroyTime);
+        Transform spawnPoint;
+        int spawn = Random.Range(1, 9);
+        spawnPoint = transform.parent.GetChild(1).GetChild(spawn);
+        transform.position = new Vector2(spawnPoint.position.x, spawnPoint.position.y);       
+    }
 }
