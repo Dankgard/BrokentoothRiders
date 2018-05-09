fe30816b39c8a0ab8b5c475cdf1557f67f31b814
Dankgard <dangar18@ucm.es>
2018-05-09 10:45:57 +0200

    Empezando sonidos de sangre

--- Assets/scripts/Bala.cs
+++ Assets/scripts/Bala.cs
@@ -6,6 +6,7 @@ public class Bala : MonoBehaviour {
 
     public int bulletDamage;
 
+    public GameObject[] blood = new GameObject[7];
     public GameObject blood1;
     public GameObject blood2;
     public GameObject blood3;
@@ -17,6 +18,28 @@ public class Bala : MonoBehaviour {
 
     int bloodType;
     public float bloodTime;
+
+    AudioSource source;
+    public AudioClip bl1;
+    public AudioClip bl2;
+    public AudioClip bl3;
+    public AudioClip bl4;
+    public AudioClip bl5;
+    public AudioClip bl6;
+    public AudioClip bl7;
+    public AudioClip bl8;
+    public AudioClip bl9;
+    public AudioClip bl10;
+    public AudioClip bl11;
+    public AudioClip bl12;
+    public AudioClip bl13;
+    AudioClip bloodSound;
+    int bloodSoundType;
+
+    void Awake()
+    {
+        source = GetComponent<AudioSource>();
+    }
 
 
     void OnTriggerEnter2D(Collider2D collider)
@@ -54,6 +77,52 @@ public class Bala : MonoBehaviour {
 
     void Bleed()
     {
+        bloodSoundType = Random.Range(1, 14);
+
+        switch (bloodSoundType)
+        {
+            case 1:
+                bloodSound = bl1;
+                break;
+            case 2:
+                bloodSound = bl2;
+                break;
+            case 3:
+                bloodSound = bl3;
+                break;
+            case 4:
+                bloodSound = bl4;
+                break;
+            case 5:
+                bloodSound = bl5;
+                break;
+            case 6:
+                bloodSound = bl6;
+                break;
+            case 7:
+                bloodSound = bl7;
+                break;
+            case 8:
+                bloodSound = bl8;
+                break;
+            case 9:
+                bloodSound = bl9;
+                break;
+            case 10:
+                bloodSound = bl10;
+                break;
+            case 11:
+                bloodSound = bl11;
+                break;
+            case 12:
+                bloodSound = bl12;
+                break;
+            case 13:
+                bloodSound = bl13;
+                break;
+        }
+        source.PlayOneShot(bl1,1.0f);//bloodSound,1.0f);
+
         bloodType = Random.Range(1, 8);
         switch(bloodType)
         {
---
+++ Assets/soundtest.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class soundtest : MonoBehaviour {
+
+    public AudioClip c;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.anyKeyDown)
+        {
+            AudioSource s = gameObject.GetComponent<AudioSource>();
+            s.PlayOneShot(c,1.0f);
+        }
+	}
+}
