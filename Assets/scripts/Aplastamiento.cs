663c7ff4262e5e59256e721210d654aa5b6507ec
Dankgard <dangar18@ucm.es>
2018-05-23 14:01:18 +0200

    Revert "Arreglando algunos errores de salto y de nivel"
    This reverts commit 3dc78547a1111d9128560956f53d023be53490a5.

--- Assets/scripts/Aplastamiento.cs
+++ Assets/scripts/Aplastamiento.cs
@@ -2,9 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Aplastamiento : MonoBehaviour {
+public class Aplastamiento : MonoBehaviour {   
 
+<<<<<<< HEAD
+<<<<<<< HEAD
     public GameObject jugador;	
+=======
+    
+>>>>>>> parent of 3dc7854... Arreglando algunos errores de salto y de nivel
+=======
+    
+>>>>>>> parent of 3dc7854... Arreglando algunos errores de salto y de nivel
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
