cb9bce94b2688a70ed1c20148f843647e5be0d3b
Dankgard <dangar18@ucm.es>
2018-05-23 14:02:38 +0200

    Revert de revert

--- Assets/scripts/Aplastamiento.cs
+++ Assets/scripts/Aplastamiento.cs
@@ -6,6 +6,7 @@ public class Aplastamiento : MonoBehaviour {
 
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
     public GameObject jugador;	
 =======
     
@@ -13,6 +14,17 @@ public class Aplastamiento : MonoBehaviour {
 =======
     
 >>>>>>> parent of 3dc7854... Arreglando algunos errores de salto y de nivel
+=======
+    public bool edificio = false;
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            edificio = true;
+            Debug.Log("funciona");
+        }
+    }
+>>>>>>> parent of d176b78... Revert "Merge remote-tracking branch 'origin/Gonzalo-Alejandro'"
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
@@ -21,6 +33,7 @@ public class Aplastamiento : MonoBehaviour {
             Debug.Log("Muerte");
             GameManager.instance.Death();
         }
+        
     }   
 
 }
