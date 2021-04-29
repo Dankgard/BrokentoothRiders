173c0f70e437afa69f7708a9e2caec54589d71bf
Zhefan <miguelye@ucm.es>
2021-04-29 17:53:57 +0200

    FEAT: Metrics added

---
+++ docs/Tracker/Tracker/Metric.cs
@@ -0,0 +1,129 @@
+﻿using System.Collections.Generic;
+
+namespace Tracker
+{
+    public class Vector2
+    {
+        public float X { get; set; }
+        public float Y { get; set; }
+    }
+
+    public class DamageFrequency
+    {
+        public DamageFrequency()
+        {
+            Metric = "Damage Frequency";
+            Positions = new List<Vector2>();
+        }
+        public string Metric { get; set; }
+        public int Level { get; set; }
+        public List<Vector2> Positions;
+
+        public void AddPosition(float posX, float posY)
+        {
+            Positions.Add(new Vector2 { X = posX, Y = posY });
+        }
+    }
+    public class HitFrequency
+    {
+        public HitFrequency()
+        {
+            Metric = "Hit Frequency";
+            HitPerEnemy = new();
+        }
+        public string Metric { get; set; }
+        public int Level { get; set; }
+        public Dictionary<string, int> HitPerEnemy;
+        public void AddEntry(string enemyType)
+        {
+            if (HitPerEnemy.ContainsKey(enemyType))
+            {
+                HitPerEnemy[enemyType]++;
+            }
+            else
+            {
+                HitPerEnemy.Add(enemyType, 1);
+            }
+        }
+    }
+    public class LevelTime
+    {
+        public LevelTime(float InitialTimer)
+        {
+            Metric = "Level Time";
+            Time = InitialTimer;
+        }
+        public string Metric { get; set; }
+        public float Time { get; set; }
+
+        public void StopTimer(float FinalTimer)
+        {
+            Time = FinalTimer - Time;
+        }
+    }
+    public class WeaponUsageFrequency
+    {
+        public WeaponUsageFrequency()
+        {
+            Metric = "Weapon Usage Frequency";
+            WeaponUsage = new();
+        }
+        public string Metric { get; set; }
+        public int Level { get; set; }
+        public Dictionary<string, float> WeaponUsage;
+
+        //el tiempo que recibe en el parametro timeUsed se controla desde unity
+        public void AddEntry(string gunType, float timeUsed)
+        {
+            if (WeaponUsage.ContainsKey(gunType))
+            {
+                WeaponUsage[gunType] += timeUsed;
+            }
+            else
+            {
+                WeaponUsage.Add(gunType, timeUsed);
+            }
+        }
+    }
+    public class WeaponAccuracy
+    {
+        public WeaponAccuracy()
+        {
+            Metric = "Weapon Accuracy";
+            WeaponHit = new();
+            WeaponMiss = new();
+            WeaponAccuracyPercentage = new();
+        }
+        public string Metric { get; set; }
+        public int Level { get; set; }        
+        public Dictionary<string, float> WeaponHit;
+        public Dictionary<string, float> WeaponMiss;
+        public Dictionary<string, float> WeaponAccuracyPercentage;
+
+        public void AddHitEntry(string gunType, bool hit)
+        {
+            if (hit)
+            {
+                if (WeaponHit.ContainsKey(gunType))
+                    WeaponHit[gunType]++;
+                else WeaponHit.Add(gunType, 1);
+            }
+            else
+            {
+                if (WeaponMiss.ContainsKey(gunType))
+                    WeaponMiss[gunType]++;
+                else WeaponMiss.Add(gunType, 1);
+            }
+        }
+        public void CalculateAccuracy()
+        {
+            foreach (KeyValuePair<string, float> entry in WeaponHit)
+            {
+                if (WeaponAccuracyPercentage.ContainsKey(entry.Key))
+                    WeaponAccuracyPercentage[entry.Key] = WeaponHit[entry.Key] / (WeaponHit[entry.Key] + WeaponMiss[entry.Key]);
+                else WeaponAccuracyPercentage.Add(entry.Key, WeaponHit[entry.Key] / (WeaponHit[entry.Key] + WeaponMiss[entry.Key]));
+            }
+        }
+    }
+
+}
--- docs/Tracker/Tracker/Program.cs
+++ docs/Tracker/Tracker/Program.cs
@@ -1,33 +1,47 @@
 ﻿using Newtonsoft.Json;
 using System;
 using System.IO;
-using System.Text;
 using System.Collections.Generic;
-using System.Web;
 
 namespace Tracker
 {
-    public class Account
+    class Program
+    {
+        static void Main() {
+
+            HitFrequency hitFrequency = new();
+            hitFrequency.AddEntry("Rifle");
+            hitFrequency.Level = 1;
+            string json = JsonConvert.SerializeObject(hitFrequency, Formatting.Indented);
+            File.WriteAllText("../Files/hitFrequency.json", json);
+
+            string jso2n = JsonConvert.SerializeObject(hitFrequency, Formatting.Indented);
+            HitFrequency acc = JsonConvert.DeserializeObject<HitFrequency>(jso2n);
+        }
+    }
+}
+
+
+
+
+/*
+ * 
+ *     public class Account
     {
         public string Email { get; set; }
         public bool Active { get; set; }
         public DateTime CreatedDate { get; set; }
         public IList<string> Roles { get; set; }
     }
-
-    class Program
-    {
-        static void Main() {
             Account account = new Account
             {
                 Email = "lope@example.com",
                 Active = true,
                 CreatedDate = new DateTime(2013, 1, 20, 0, 0, 0, DateTimeKind.Utc),
-                Roles = new List<string>
-    {
-        "User 121321",
-        "Admin"
-    }
+                Roles = new List<string> {
+                    "User 121321",
+                    "Admin"
+                }
             };
 
             string json = JsonConvert.SerializeObject(account, Formatting.Indented);
@@ -38,6 +52,6 @@ namespace Tracker
             // Deserealizar
             Account acc = JsonConvert.DeserializeObject<Account>(json2);
             Console.WriteLine(acc.Email);
-        }
-    }
-}
+
+ 
+ */
