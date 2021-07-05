b95198fa32fb1bc26a62e4846b18e6f83421dfe6
GonzaPM7 <gonzap03@ucm.es>
2021-07-05 19:43:54 +0200

    Persistencia hecha (seguramente haya que modificar cosas)

---
+++ Tracker/Tracker/FilePersistence.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace Tracker
+{
+    class FilePersistence : IPersistence
+    {
+
+        string path;
+
+        public FilePersistence(ISerializer s, string path) : base(s)
+        {
+            this.path = path;
+        }
+
+        public override void flush()
+        {
+            string inf = "";
+            foreach (TrackerEvent e in events)
+            {
+                inf += serializer_.serialize(e);
+            }
+
+            if (File.Exists(path))
+            {
+                string temp = File.ReadAllText(path);
+                temp += inf;
+                File.WriteAllText(path, temp);
+            }
+            else
+                File.WriteAllText(path, inf);
+
+            clearEvents();
+        }
+
+        public override void send(TrackerEvent e)
+        {
+            events.Enqueue(e);
+        }
+    }
+}
---
+++ Tracker/Tracker/IPersistence.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace Tracker
+{
+    public abstract class IPersistence
+    {
+        public ISerializer serializer_ = null;
+        public Queue<TrackerEvent> events = new Queue<TrackerEvent>();
+
+        protected IPersistence(ISerializer serializer)
+        {
+            serializer_ = serializer;
+        }
+
+        public abstract void send(TrackerEvent e);
+        public abstract void flush();
+
+        public void setSerializer(ISerializer s)
+        {
+            serializer_ = s;
+        }
+
+        public void clearEvents()
+        {
+            events.Clear();
+        }
+    }
+}
